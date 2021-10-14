5205ac80a633fa4480cded53150e7d257bd9c29e
Sebastian Dröge <sebastian@centricular.com>
2021-10-14 23:51:19 +0300

    Use widget.grab_focus() instead of window.set_focus_widget()
    Has the same effect but according to the docs is more recommended.

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ fn build_ui(application: &gtk::Application) {
     let widget = widget::Widget::new();
     window.set_child(Some(&widget));
 
+    widget.grab_focus();
+
     window.set_default_size(800, (800.0 / 1.75) as i32);
     window.set_title(Some("Mandelbrot"));
-
-    window.set_focus_widget(Some(&widget));
 
     window.connect_close_request(move |win| {
         win.close();
