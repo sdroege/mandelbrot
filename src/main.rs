456fad75cf744465587d5c567902016078e8fd81
Florian "sp1rit"​ <sp1rit@disroot.org>
2026-03-31 20:08:59 +0200

    android: support building for android

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,15 @@ fn make_application() -> gio::Application {
     application.upcast()
 }
 
+#[cfg(not(target_os = "android"))]
 fn main() -> gtk::glib::ExitCode {
     let application = make_application();
     application.run()
+}
+
+#[cfg(target_os = "android")]
+#[unsafe(no_mangle)]
+pub extern "C" fn main(argc: i32, argv: *mut *mut std::ffi::c_char) -> i32 {
+    let application = make_application();
+    unsafe { gio::ffi::g_application_run(application.as_ptr(), argc, argv) }
 }
